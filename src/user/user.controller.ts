53d6bded20c7b36b419fc915487b41d11612a6de
Javo <javier.cervera@coverwallet.com>
2023-08-11 10:45:12 +0200

    first project commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(
+    @Body('name') userName: string,
+    @Body('email') userEmail: string,
+    @Body('age') userAge: number,
+  ) {
+    const generatedId = this.userService.create(userName, userEmail, userAge);
+    return { id: generatedId, name: userName, email: userEmail, age: userAge };
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.userService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.update(+id, updateUserDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.userService.remove(+id);
+  }
+}
